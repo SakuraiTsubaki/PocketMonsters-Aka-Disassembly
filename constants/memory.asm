b438d3fbf15493ef731414c5e64d6cf3c6c109d2
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 18:01:53 +0900

    Add Japanese text-engine RAM symbols

--- constants/memory.asm
+++ constants/memory.asm
@@ -1,6 +1,7 @@
 ; RAM/HRAM addresses verified from reconstructed Bank 00 machine code.
 
 DEF hROMBankTemp              EQU $FF8B
+DEF hTextID                   EQU $FF8C
 DEF hJoyHeld                  EQU $FFB4
 DEF hJoy5                     EQU $FFB5
 DEF hLoadedROMBank            EQU $FFB8
@@ -14,13 +15,21 @@ DEF hVBlankCopyDest           EQU $FFC9
 DEF hVBlankCopyDoubleSize     EQU $FFCB
 DEF hVBlankCopyDoubleSource   EQU $FFCC
 DEF hVBlankCopyDoubleDest     EQU $FFCE
+DEF hWhoseTurn                EQU $FFF3
 
-DEF wShadowOAM     EQU $C300
-DEF wShadowOAMEnd  EQU $C3A0
+DEF wShadowOAM                EQU $C300
+DEF wShadowOAMEnd             EQU $C3A0
 
 IF DEF(BUILD_JP)
-	DEF wTileMap EQU $C3A0
-	DEF wBuffer  EQU $CEE4
+	DEF wTileMap                  EQU $C3A0
+	DEF wBattleMonNick            EQU $CFF0
+	DEF wEnemyMonNick             EQU $CFC1
+	DEF wTextDest                 EQU $CC3A
+	DEF wLinkState                EQU $D0F0
+	DEF wPlayerName               EQU $D11D
+	DEF wLetterPrintingDelayFlags EQU $D2D7
+	DEF wRivalName                EQU $D2CE
+	DEF wBuffer                   EQU $CEE4
 ENDC
 
 IF DEF(BUILD_WEST)
