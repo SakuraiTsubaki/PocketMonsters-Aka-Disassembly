6e2bb61f0f4317d3b2a67f554917fe955cb3c4d5
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 13:53:41 +0900

    Add Japanese Bank 00 RAM symbols

--- constants/memory.asm
+++ constants/memory.asm
@@ -1,9 +1,13 @@
-; RAM addresses verified from the western Red Bank 00 machine code.
-; Japanese RAM symbols will be added as their Bank 00 routines are recovered.
+; RAM addresses verified from the reconstructed Bank 00 machine code.
+
+DEF hLoadedROMBank EQU $FFB8
+DEF wShadowOAM     EQU $C300
+DEF wShadowOAMEnd  EQU $C3A0
+
+IF DEF(BUILD_JP)
+	DEF wBuffer EQU $CEE4
+ENDC
 
 IF DEF(BUILD_WEST)
-	DEF hLoadedROMBank EQU $FFB8
-	DEF wShadowOAM     EQU $C300
-	DEF wShadowOAMEnd  EQU $C3A0
-	DEF wBuffer        EQU $CEE9
+	DEF wBuffer EQU $CEE9
 ENDC
