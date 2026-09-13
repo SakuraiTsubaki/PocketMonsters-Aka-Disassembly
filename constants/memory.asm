3f61f4759d62e173587ebaf0915722325cc26348
SakuraiTsubaki <nokang3@naver.com>
2026-09-13 17:25:01 +0900

    Add verified Japanese Bank 00 RAM symbols

--- constants/memory.asm
+++ constants/memory.asm
@@ -1,11 +1,26 @@
-; RAM addresses verified from the reconstructed Bank 00 machine code.
+; RAM/HRAM addresses verified from reconstructed Bank 00 machine code.
 
-DEF hLoadedROMBank EQU $FFB8
+DEF hROMBankTemp              EQU $FF8B
+DEF hJoyHeld                  EQU $FFB4
+DEF hJoy5                     EQU $FFB5
+DEF hLoadedROMBank            EQU $FFB8
+DEF hAutoBGTransferEnabled    EQU $FFBA
+DEF hVBlankCopyBGSource       EQU $FFC1
+DEF hVBlankCopyBGDest         EQU $FFC3
+DEF hVBlankCopyBGNumRows      EQU $FFC5
+DEF hVBlankCopySize           EQU $FFC6
+DEF hVBlankCopySource         EQU $FFC7
+DEF hVBlankCopyDest           EQU $FFC9
+DEF hVBlankCopyDoubleSize     EQU $FFCB
+DEF hVBlankCopyDoubleSource   EQU $FFCC
+DEF hVBlankCopyDoubleDest     EQU $FFCE
+
 DEF wShadowOAM     EQU $C300
 DEF wShadowOAMEnd  EQU $C3A0
 
 IF DEF(BUILD_JP)
-	DEF wBuffer EQU $CEE4
+	DEF wTileMap EQU $C3A0
+	DEF wBuffer  EQU $CEE4
 ENDC
 
 IF DEF(BUILD_WEST)
